86a54f95ca98bd424dbfcfb7ad60c04a3a9c33f8
Daniel Silva <dfsilva.dxp@gmail.com>
2023-10-21 09:52:13 -0300

    feat: adiciona o componente Flex.

---
+++ src/components/Flex/styles.ts
@@ -0,0 +1,17 @@
+'use client';
+
+import styled, { css } from 'styled-components';
+
+import { FlexType } from './types';
+
+export const FlexContent = styled.div<FlexType>`
+  ${({ display, direction, align, justify, content, wrap, gap }) => css`
+    display: ${display};
+    flex-direction: ${direction};
+    align-items: ${align};
+    align-content: ${content};
+    justify-content: ${justify};
+    flex-wrap: ${wrap};
+    gap: ${gap};
+  `}
+`;
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { FlexComponent as Flex } from './Flex';
