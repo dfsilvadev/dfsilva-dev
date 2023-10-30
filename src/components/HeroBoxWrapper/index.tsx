eba817e073b4df916008474e4b841991420d1ce0
Daniel Silva <dfsilva.dxp@gmail.com>
2023-10-30 20:14:06 -0300

    feat: adiciona o componente HeroBoxWrapper.

---
+++ src/components/HeroBoxWrapper/index.tsx
@@ -0,0 +1,69 @@
+import Image from 'next/image';
+
+import { Box, MoveUp, SocialIcons } from '@/components';
+
+import * as S from './styles';
+
+const HeroBoxWrapper = () => {
+  return (
+    <S.HeroBoxWrapperContent>
+      <Box
+        splitChildren="React"
+        area="react"
+        borderbat="top"
+        borderlar="left"
+        icon={
+          <Image
+            src="images/svg/icons/react_icon.svg"
+            alt="React logo"
+            width={24}
+            height={24}
+            priority
+          />
+        }
+      >
+        <MoveUp data-wordanimated="into">React</MoveUp>
+      </Box>
+
+      <Box
+        splitChildren="Next"
+        area="next"
+        borderbat="top"
+        borderlar="left"
+        icon={
+          <Image
+            src="images/svg/icons/next_icon.svg"
+            alt="Next logo"
+            width={24}
+            height={24}
+            priority
+          />
+        }
+      >
+        <MoveUp data-wordanimated="into">Next</MoveUp>
+      </Box>
+
+      <Box
+        splitChildren="Node"
+        area="node"
+        borderbat="top"
+        borderlar="left"
+        icon={
+          <Image
+            src="images/svg/icons/node_icon.svg"
+            alt="Node logo"
+            width={24}
+            height={24}
+            priority
+          />
+        }
+      >
+        <MoveUp data-wordanimated="into">Node</MoveUp>
+      </Box>
+
+      <SocialIcons />
+    </S.HeroBoxWrapperContent>
+  );
+};
+
+export default HeroBoxWrapper;
